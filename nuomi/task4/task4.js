7eb6d25ed2e6f000ac777608fa7bf790ec5d97a7
minfield <reFugd3e@gmail.com>
2017-03-06 09:34:49 +0800

    task4 直接拼凑网址爬取页面

--- nuomi/task4/task4.js
+++ nuomi/task4/task4.js
@@ -1,26 +1,71 @@
-phantom.outputEncoding = 'utf-8'; 
-var page = require('webpage').create(); 
-page.open("http://baidu.com", function(status){
-    console.log("here"); 
-    console.log("status: " + status);
-    page.evaluate(function(text){
-        document.getElementById("kw").value = text; 
-        document.getElementById("su").click(); 
-    }, "ife");  
-    // phantom.exit(); 
-});
-
-page.onConsoleMessage = function(msg){
-    console.log(msg); 
+var page = require("webpage").create(); 
+var system = require("system"); 
+// console.log(0); 
+if(system.args.length === 1){
+    console.log("Usage: phantomjs task4.js <some keyword>"); 
+    phantom.exit(); 
 }
 
-page.onResourceReceived = function(response){
-    // if(response.stage == "end"){
-    //     console.log("url  " + response.url); 
-    // }
-    //搜索结束条件
-   if (){
-        page.render(render.png); 
-        phantom.exit(); 
-   }
+// console.log(1); 
+var result = {
+       code: 0, 
+       msg: "",
+       word: system.args[1],
+       time: Date.now(), 
+       dataList: []
 }
+
+// console.log(2); 
+var url = "https://baidu.com/s?wd=" + result.word; 
+// page.open(url, function(status){
+//     page.render("baidu.png"); 
+//     console.log(status); 
+// })
+page.open(url, function(status){
+    if(status !== "success")
+        doFail(); 
+    else
+        doSuccess(); 
+}); 
+
+function doFail(){
+    result.msg = "capture failed"; 
+    result.time = -1; 
+    console.log(JSON.stringify(result)); 
+    phantom.exit(); 
+}
+
+function doSuccess(){
+    page.render("ife.png"); 
+    result.time = Date.now() - result.time; 
+    result.dataList = page.evaluate(function(){
+        var containers = document.getElementsByClassName("c-container"); 
+        var dataList = new Array(); 
+        for(var i = 0; i < containers.length; i++){
+            var ele = containers[i];  
+            var link = ele.getElementsByClassName("t")[0].getElementsByTagName("a")[0]; 
+            var abstract = ele.getElementsByClassName("c-abstract")[0] || new Object(); 
+            var pic = ele.getElementsByClassName("c-img")[0]; 
+            if(pic){
+                pic = pic.getAttribute("src"); 
+            }
+            dataList.push({
+                title: link.innerText || "", 
+                info: abstract.innerText || "",
+                link: link.getAttribute("href") || "", 
+                pic: pic || ""
+            });
+        } 
+        return dataList; 
+    }); 
+    result.code = 1; 
+    result.msg = "capture success"; 
+    console.log(JSON.stringify(result)); 
+    phantom.exit();  
+}
+
+// page.onConsoleMessage = function(msg){
+//     console.log(msg); 
+// }
+
+// 一个空对象的属性引用和函数调用的返回结果分别是什么
--- nuomi/task4/test.js
+++ nuomi/task4/test.js
@@ -1,50 +1,147 @@
-var webPage = require('webpage');
-var page = webPage.create();
+// The purpose of this is to show how and when events fire, considering 5 steps
+// happening as follows:
+//
+//      1. Load URL
+//      2. Load same URL, but adding an internal FRAGMENT to it
+//      3. Click on an internal Link, that points to another internal FRAGMENT
+//      4. Click on an external Link, that will send the page somewhere else
+//      5. Close page
+//
+// Take particular care when going through the output, to understand when
+// things happen (and in which order). Particularly, notice what DOESN'T
+// happen during step 3.
+//
+// If invoked with "-v" it will print out the Page Resources as they are
+// Requested and Received.
+//
+// NOTE.1: The "onConsoleMessage/onAlert/onPrompt/onConfirm" events are
+// registered but not used here. This is left for you to have fun with.
+// NOTE.2: This script is not here to teach you ANY JavaScript. It's aweful!
+// NOTE.3: Main audience for this are people new to PhantomJS.
 
-var fs = require('fs');
-//上文介绍的文件系统 输出文件需要
-var path = 'url.txt';
+"use strict";
+var sys = require("system"),
+    page = require("webpage").create(),
+    logResources = false,
+    step1url = "http://en.wikipedia.org/wiki/DOM_events",
+    step2url = "http://en.wikipedia.org/wiki/DOM_events#Event_flow";
 
-//在PhantomJS中 执行JS函数的时候是不能直接使用Phantom的方法的。需要hook住alert方法，再使用alert来输出
-page.onAlert = function(msg) {
-    fs.write(path, msg, "w");
-};
+if (sys.args.length > 1 && sys.args[1] === "-v") {
+    logResources = true;
+}
 
-//引入jquery 让我们的操作更方便。
-page.injectJs("jquery-1.6.1.min.js");
-page.open("http://www.baidu.com", function(status) {
-
-    //当打开成功后。输入检索内容并点击搜索
-    //注意这两个按钮的的ID还是需要人为去看一下的
-    console.log(status); 
-    if ( status === "success" ) {
-        page.evaluate(function(text) {
-            $("#kw").val(text);
-            $("#su").click();
-        }, "hello world");
-        
+function printArgs() {
+    var i, ilen;
+    for (i = 0, ilen = arguments.length; i < ilen; ++i) {
+        console.log("    arguments[" + i + "] = " + JSON.stringify(arguments[i]));
     }
+    console.log("");
+}
 
-});
+////////////////////////////////////////////////////////////////////////////////
 
-//我们需要获取资源加载成功的方法并重写
-//更多的phantom的方法可以参考官方链接。
-page.onResourceReceived = function (res,network) {
-    console.log(res.url); 
-    if (res.stage == "end") {
-        //我们仍然需要知道baidu搜索后的结束条件
-        if (res.url.indexOf("b1.bdstatic.com")>=0) {
-            //获取左边的所有标签并打印出text和链接
-            page.evaluate(function() {
-                var result="";
-                $("#content_left a").each(function(){
-                    result=result+$(this).text()+"   "+$(this).attr("href")+"\n";
-                });
-                alert(result);
-            });
-
-            page.render('baidu.jpeg');
-            phantom.exit();
-        }
-    }
+page.onInitialized = function() {
+    console.log("page.onInitialized");
+    printArgs.apply(this, arguments);
 };
+page.onLoadStarted = function() {
+    console.log("page.onLoadStarted");
+    printArgs.apply(this, arguments);
+};
+page.onLoadFinished = function() {
+    console.log("page.onLoadFinished");
+    printArgs.apply(this, arguments);
+};
+page.onUrlChanged = function() {
+    console.log("page.onUrlChanged");
+    printArgs.apply(this, arguments);
+};
+page.onNavigationRequested = function() {
+    console.log("page.onNavigationRequested");
+    printArgs.apply(this, arguments);
+};
+// page.onRepaintRequested = function() {
+//     console.log("page.onRepaintRequested");
+//     printArgs.apply(this, arguments);
+// };
+
+if (logResources === true) {
+    page.onResourceRequested = function() {
+        console.log("page.onResourceRequested");
+        printArgs.apply(this, arguments);
+    };
+    page.onResourceReceived = function() {
+        console.log("page.onResourceReceived");
+        printArgs.apply(this, arguments);
+    };
+}
+
+// page.onClosing = function() {
+//     console.log("page.onClosing");
+//     printArgs.apply(this, arguments);
+// };
+
+// // window.console.log(msg);
+// page.onConsoleMessage = function() {
+//     console.log("page.onConsoleMessage");
+//     printArgs.apply(this, arguments);
+// };
+
+// // window.alert(msg);
+// page.onAlert = function() {
+//     console.log("page.onAlert");
+//     printArgs.apply(this, arguments);
+// };
+// // var confirmed = window.confirm(msg);
+// page.onConfirm = function() {
+//     console.log("page.onConfirm");
+//     printArgs.apply(this, arguments);
+// };
+// // var user_value = window.prompt(msg, default_value);
+// page.onPrompt = function() {
+//     console.log("page.onPrompt");
+//     printArgs.apply(this, arguments);
+// };
+
+////////////////////////////////////////////////////////////////////////////////
+
+setTimeout(function() {
+    console.log("");
+    console.log("### STEP 1: Load '" + step1url + "'");
+    page.open(step1url);
+}, 0);
+
+setTimeout(function() {
+    console.log("");
+    console.log("### STEP 2: Load '" + step2url + "' (load same URL plus FRAGMENT)");
+    page.open(step2url);
+}, 5000);
+
+setTimeout(function() {
+    console.log("");
+    console.log("### STEP 3: Click on page internal link (aka FRAGMENT)");
+    page.evaluate(function() {
+        var ev = document.createEvent("MouseEvents");
+        ev.initEvent("click", true, true);
+        document.querySelector("a[href='#Event_object']").dispatchEvent(ev);
+    });
+}, 10000);
+
+setTimeout(function() {
+    console.log("");
+    console.log("### STEP 4: Click on page external link");
+    page.evaluate(function() {
+        var ev = document.createEvent("MouseEvents");
+        ev.initEvent("click", true, true);
+        document.querySelector("a[title='JavaScript']").dispatchEvent(ev);
+    });
+}, 15000);
+
+setTimeout(function() {
+    console.log("");
+    console.log("### STEP 5: Close page and shutdown (with a delay)");
+    page.close();
+    setTimeout(function(){
+        phantom.exit();
+    }, 100);
+}, 20000);
